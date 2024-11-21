d482c25e3781beb10c1782593ef8c56c92ad3d12
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-11-21 17:17:44 +0200

    Practice

---
+++ src/main/java/com/exponent_softwares/justpracticecode/JustPracticeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.exponent_softwares.justpracticecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustPracticeCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustPracticeCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exponent_softwares/justpracticecode/JustPracticeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exponent_softwares.justpracticecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustPracticeCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
